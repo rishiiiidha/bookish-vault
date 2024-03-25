7b88dc387a06e6d344451dab0ab9202fc758e861
Rishidha Addanki <rishidha04@gmail.com>
2024-03-25 12:40:47 +0530

    updated-backend-files

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World")
+  res.json("Hello World")
 })
 app.use(cors());
 app.use('/book', booksRoute);
